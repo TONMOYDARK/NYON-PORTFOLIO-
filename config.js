26e563f7352f2e9af4b45205f48922f1374e82d8
TONMOYDARK <63608431+TONMOYDARK@users.noreply.github.com>
2026-09-14 12:13:05 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Supabase public configuration.
 // Replace these two values with your Supabase Project URL and anon/publishable key.
-window.SUPABASE_URL = 'https://YOUR-PROJECT.supabase.co';
-window.SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY';
+window.SUPABASE_URL = 'https://jhhbxscdltqdnsorxeck.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_Mve9eEdv5x59VCX1eBUTdg_LB3TN7hX';
